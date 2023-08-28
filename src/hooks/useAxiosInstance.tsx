36a46cd82ef880cf835d32abad4e40da8b23e111
Juan Rozo <juan.rozo89@gmail.com>
2023-08-28 14:43:51 -0500

    fix: axios instance timeout sometimes having unpredictable behavior in useAxiosInstance.tsx\nstyle: error handling in alertFunctions.tsx to inform of server delay when axiosInstance reaches timeout

--- src/hooks/useAxiosInstance.tsx
+++ src/hooks/useAxiosInstance.tsx
@@ -1,9 +1,19 @@
 import { REQ_TIMEOUT_CLIENT } from "../constants";
 import axios from "axios";
 
-const useAxiosInstance = () => {
-  const axiosInstance = axios.create();
+/*const useAxiosInstance = () => {
+  const axiosInstance =
+    axios.create();
   axiosInstance.defaults.signal = AbortSignal.timeout(REQ_TIMEOUT_CLIENT);
+  return axiosInstance;
+};*/
+
+const useAxiosInstance = () => {
+  //const controller = new AbortController();
+  const axiosInstance = axios.create({
+    timeout: REQ_TIMEOUT_CLIENT,
+  });
+  //axiosInstance.defaults.cancelToken = controller.signal;
   return axiosInstance;
 };
 
