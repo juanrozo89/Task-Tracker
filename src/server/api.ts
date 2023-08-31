e9841b51341faaaefe9987ee55276d0332b5581d
Juan Rozo <juan.rozo89@gmail.com>
2023-08-31 11:25:06 -0500

    feat: add route for recovering password in api.ts using nodemailer to send a mail to the client

--- src/server/api.ts
+++ src/server/api.ts
@@ -24,6 +24,8 @@ dotenv.config();
 import bcrypt from "bcrypt";
 const saltRounds = 10;
 
+import crypto from "crypto";
+
 import mongoose from "mongoose";
 mongoose
   .connect(process.env.MONGO_URI!)
@@ -77,6 +79,18 @@ const userSchema = new Schema({
 });
 
 const User = mongoose.model("User", userSchema);
+
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env["EMAIL"],
+    pass: process.env["EMAIL_PASSWORD"],
+  },
+});
 
 // COMMON ERRORS:
 const missingFieldError = (field: string, res: Response) => {
@@ -200,6 +214,31 @@ const attemptPasswordLimiter = rateLimit({
 
 export default function (app: Express) {
   // ~~~ HANDLE USER SESSION ~~~
+
+  // recover password
+  app
+    .route("/api/recover-password")
+    .post(async (req: Request, res: Response) => {
+      const resetToken = crypto.randomBytes(32).toString("hex");
+      req.session.user = `${resetToken}`;
+      const resetLink = `${req.protocol}://${req.get(
+        "host"
+      )}/reset-password?token=${resetToken}`;
+      try {
+        const email = req.body.email;
+        const mailOptions = {
+          from: process.env["EMAIL"],
+          to: email,
+          subject: "Password Recovery",
+          text: `Click on ${resetLink} to reset your password`,
+        };
+        await transporter.sendMail(mailOptions);
+        res.status(200).json({ message: "Email sent successfully." });
+      } catch (error) {
+        console.error("Error sending email: ", error);
+        res.status(500).json({ error: "Failed to send email." });
+      }
+    });
 
   // get user from session:
   app
